70aed9a869b94e38f0bcd13fea6d4c3495964832
Mokdad5565 <134150178+Mokdad5565@users.noreply.github.com>
2024-02-07 11:34:57 +0200

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,69 @@
 #!/usr/bin/python3
-'''function print a text in new lines'''
+"""
+
+This module prints a text with a 2 new lines after each of
+these characters: `.`, `?`, `:`
+
+Example:
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.$
+    $
+    Quonam modo?$
+    $
+    Utrum igitur tibi litteram videor an totas paginas commovere?$
+    $
+    Non autem hoc:$
+    $
+
+    * text must be a string
+
+    * There should be no space at the beginning or
+    at the end of each printed line
+
+"""
 
 
 def text_indentation(text):
-    '''validation'''
-    if type(text) != str:
-        raise TypeError("text must be a string")
-    '''chars'''
-    char = [".", "?", ":"]
-    '''text iterates'''
-    prev = 0
-    for i in range(len(text)):
-        '''validation'''
-        if i == len(text) - 1:
-            print(text[prev:i + 1], end="")
-        elif text[i] in char:
-            print(text[prev:i + 1], end="")
-            print('\n')
-            prev = i + 1
-            while text[prev] == " ":
-                prev += 1
+    """
+
+    Prints a text with indentation
+
+    Args:
+        text (str): The text to prints.
+
+    Raises:
+        TypeError: If `text` isn't string.
+
+    """
+
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    text_length = len(text)
+    idx = 0
+    new_string = ''
+    starting = True
+
+    while idx < text_length:
+        if text[idx] == ' ' and starting is True:
+            idx += 1
+            continue
+
+        starting = False
+
+        if text[idx] in '.?:':
+            new_string += text[idx]
+            new_string += '\n'
+            new_string += '\n'
+            idx += 1
+
+            while idx < text_length and text[idx] == ' ':
+                idx += 1
+
+            continue
+
+        if idx < text_length:
+            new_string += text[idx]
+            idx += 1
+
+    print(new_string, end='')
